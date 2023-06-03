8e6d35ae10575cec95e772af99af4250842aa1ca
lliioollzh <lliiooll@126.com>
2023-06-03 13:32:28 +0800

    让用户自己重启避免数据写入失败

--- app/src/main/java/cn/lliiooll/ppbuff/hook/zuiyouLite/ZuiYouLiteEulaHook.kt
+++ app/src/main/java/cn/lliiooll/ppbuff/hook/zuiyouLite/ZuiYouLiteEulaHook.kt
@@ -13,6 +13,7 @@ import cn.lliiooll.ppbuff.utils.debug
 import cn.lliiooll.ppbuff.utils.dump
 import cn.lliiooll.ppbuff.utils.findClass
 import cn.lliiooll.ppbuff.utils.sync
+import cn.lliiooll.ppbuff.utils.toastLong
 import cn.lliiooll.ppbuff.utils.toastShort
 import com.github.kyuubiran.ezxhelper.utils.findAllConstructors
 import com.github.kyuubiran.ezxhelper.utils.findAllMethods
@@ -20,6 +21,7 @@ import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.github.kyuubiran.ezxhelper.utils.paramCount
 import com.kongzue.dialogx.dialogs.MessageDialog
+import com.kongzue.dialogx.dialogs.PopNotification
 import com.kongzue.dialogx.style.IOSStyle
 import de.robv.android.xposed.XposedHelpers
 import java.lang.RuntimeException
@@ -45,7 +47,12 @@ object ZuiYouLiteEulaHook : BaseHook(
                         .setMessage(PPBuff.loadEula())
                         .setOkButton("接受") { _, _ ->
                             PConfig.set("first_inited", false)
-                            exitProcess(0)
+                            PopNotification.build()
+                                .setStyle(IOSStyle.style())
+                                .setTitle("提示")
+                                .setMessage("重启以生效")
+                                .show(activity)
+                                .noAutoDismiss()
                             false
                         }
                         .setCancelButton("拒绝") { _, _ ->
